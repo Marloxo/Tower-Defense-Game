8114428bc452620d3e360969c7ed2df38d53082a
Marloxo <omarsabekz@gmail.com>
2016-07-11 16:10:12 +0300

    Implement Basic Enemy AI

---
+++ Tower Defense Game/Assets/Enemy.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10f;
+    private Transform target;
+    private int wavepointIndex = 0;
+    // Use this for initialization
+    void Start()
+    {
+        target = waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Calc the Vector of movement
+        Vector3 dir = target.position - transform.position;
+        //Translate: to move in this direction 
+        //Normalized to make sure that the only thing control our spedd is our speed
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    private void GetNextWayPoint()
+    {
+        if (wavepointIndex >= waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+			//This retrun to make sure the code dont go further in some cases
+            return;
+        }
+
+        wavepointIndex++;
+        target = waypoints.points[wavepointIndex];
+    }
+}
---
+++ Tower Defense Game/Assets/waypoints.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class waypoints : MonoBehaviour
+{
+
+    // Use this for initialization
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
