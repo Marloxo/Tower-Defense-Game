1cdc7bc61d9dfbdb757d4866227809177cec9edf
Marloxo <marloo-_-@hotmail.com>
2016-12-18 22:00:19 +0200

    :sparkles: add a Pause Menu to our game

--- Tower Defense Game/Assets/Scripts/CameraController.cs
+++ Tower Defense Game/Assets/Scripts/CameraController.cs
@@ -2,7 +2,6 @@
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovement = true;
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
     public float scrollSpeed = 5f;
@@ -17,12 +16,7 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
-            doMovement = !doMovement;
-
-        if (!doMovement)
-            return;
-
+ 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
---
+++ Tower Defense Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
+        {
+            Toggle();
+        }
+    }
+
+    public void Toggle()
+    {
+        pauseUI.SetActive(!pauseUI.activeSelf);
+        if (pauseUI.activeSelf)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void Retry()
+    {
+        Toggle(); //change timeScale to normal
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Menu()
+    {
+    }
+}
